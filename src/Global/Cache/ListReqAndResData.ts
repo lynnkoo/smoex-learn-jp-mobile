fe6fd15067e3a980ad72044c6ce95232c1423805
yuyingdeng <yuyingdeng@foxmail.com>
2019-12-31 14:36:15 +0800

    fix: getExposureData allProductList

--- src/Global/Cache/ListReqAndResData.ts
+++ src/Global/Cache/ListReqAndResData.ts
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { getVehAndProductList } from './ListResSelectors';
+import { getVehicleList, getBaseProductGroups } from './ListResSelectors';
 
 /** 列表页接口的请求+响应数据 */
 let reqAndResData = {
@@ -33,14 +33,20 @@ export const getExposureKeys = () => _.keys(exposureData);
 
 export const getExposureData = () => {
   const keys = getExposureKeys();
-  const { productGroups = [], vehicleList } = getVehAndProductList();
+  const vehicleList = getVehicleList() || [];
+  const productGroups = getBaseProductGroups() || [];
   const res = [];
   // 只在全部车型下面查找即可
-  const allProductList = _.get(productGroups[0], 'productList');
-  if (allProductList) {
+  const allProductList = _.reduce(productGroups, function(result, { productList }) {
+    return [
+      ...productList,
+      ...result
+    ];
+  }, []);
+  if (productGroups) {
     _.forEach(keys, (key) => {
       const [vehicleCode, bizVendorCode] = key.split('|');
-      const { vendorPriceList }: any = _.find(allProductList, { vehicleCode }) || {};
+      const { vendorPriceList }: any = _.find(allProductList, {vehicleCode}) || {};
       const vehicle = _.find(vehicleList, { vehicleCode });
       const vendor = _.find(
         vendorPriceList,

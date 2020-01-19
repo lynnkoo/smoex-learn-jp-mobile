089161c416ecf514543735981639d21bc6d55a54
t_xiao <t_xiao@ctrip.com>
2020-01-19 17:13:15 +0800

    fix android

--- src/Pages/List/Components/TipList.tsx
+++ src/Pages/List/Components/TipList.tsx
@@ -54,6 +54,7 @@ const ListTips = (props: IPropsType) => {
       paddingVertical: BbkUtils.getPixel(24),
       paddingHorizontal: BbkUtils.getPixel(32),
       backgroundColor: color.white,
+      zIndex: 1
     }}
     >
       <BbkTips
